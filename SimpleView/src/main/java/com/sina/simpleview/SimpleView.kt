2182be259aebfa5cea9225c0312ed2e143010d4a
ST <cena.tabriziyan@gmail.com>
2025-02-11 09:32:33 +0330

    1.0.11-implement confirm btm sheet dialog

--- SimpleView/src/main/java/com/sina/simpleview/SimpleView.kt
+++ SimpleView/src/main/java/com/sina/simpleview/SimpleView.kt
@@ -1,12 +1,35 @@
 package com.sina.simpleview
 
 import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import androidx.viewbinding.ViewBinding
 import com.sina.simpleview.btmsheetdialog.confirmation.ConfirmButtonStyle
 import com.sina.simpleview.btmsheetdialog.confirmation.ConfirmationBtmSheet
+import com.sina.simpleview.btmsheetdialog.normal.SimpleBtmSheetFragment
 import com.sina.simpleview.library.R
 
 object SimpleView {
     val ConfirmationBtmSheet = ConfirmationBtmSheetFactory
+    val NormalBtmSheet = CustomBtmSheetFactory
+}
+
+object CustomBtmSheetFactory {
+    fun <B : ViewBinding> create(
+        bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> B,
+        setup: (B) -> Unit
+    ): SimpleBtmSheetFragment<B> {
+        val fragment = object : SimpleBtmSheetFragment<B>(bindingInflater) {
+            override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+                super.onViewCreated(view, savedInstanceState)
+                setup(binding)
+            }
+        }
+        return fragment
+    }
 }
 
 object ConfirmationBtmSheetFactory {
