64bf4ee7afe641e39328d2392fd2fb01ef761c7c
ST <cena.tabriziyan@gmail.com>
2025-02-11 12:11:00 +0330

    1.0.21

--- app/src/main/java/com/sina/simpleviews/MainActivity.kt
+++ app/src/main/java/com/sina/simpleviews/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.sina.simpleviews
 
-import android.graphics.Color
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.sina.simpleview.SimpleView
-import com.sina.simpleview.btmsheetdialog.confirmation.ConfirmButtonStyle
 import com.sina.simpleview.library.databinding.DialogConfirmationBinding
 import com.sina.simpleviews.databinding.ActivityMainBinding
 
@@ -26,24 +23,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-//        SimpleView.ConfirmationBtmSheet.create(
-//            context = this, // Pass requireContext() if in Fragment
-//            message = "Do you want to delete this item?",
-//            positiveText = "Delete",
-//            negativeText = "Cancel",
-//            style = ConfirmButtonStyle.ROUNDED,
-//            buttonColor = Color.RED
-//        ) {
-//            Toast.makeText(this, "Item deleted", Toast.LENGTH_SHORT).show()
-//        }.show()
-
         binding.text.setOnClickListener {
-            SimpleView.SimpleDialogFragment.create<DialogConfirmationBinding>(this) {binding,dialog->
 
-            }
         }
-
     }
 
 }
