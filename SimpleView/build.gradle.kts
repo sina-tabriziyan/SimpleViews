704ba382a238badb8573b697732646d6ec5af4a8
ST <cena.tabriziyan@gmail.com>
2025-02-11 10:49:44 +0330

    1.0.17-implement confirm btm sheet dialog

--- SimpleView/build.gradle.kts
+++ SimpleView/build.gradle.kts
@@ -40,7 +40,7 @@ android {
                     from(components["release"])
                     groupId = "com.github.sina-tabriziyan"
                     artifactId = "SimpleViews"
-                    version = "1.0.16"
+                    version = "1.0.17"
                 }
             }
         }
